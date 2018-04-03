e5b2ca3a7f7108b1b4e2f64c24a80f1ea488903b
Mario Reder <m_reder10@cs.uni-kl.de>
2018-04-03 21:28:15 +0200

    chore(install): Update Windows install script to support latest Node versions

--- install.bat
+++ install.bat
@@ -1,5 +1,5 @@
 @echo off
-node --version | findstr v8 > nul
+node --version | findstr /c:v8 /c:v9 > nul
 if errorlevel 1 goto outdated
 call npm install
 if errorlevel 1 goto error
@@ -10,6 +10,6 @@ goto eof
 echo Installation has an error. Please try again or contact me on Discord: Tarnadas#0582 or https://discord.gg/SPZsgSe
 goto eof
 :outdated
-echo Your Node version is outdated. Please install Node v8.x -- https://nodejs.org
+echo Your Node version is outdated. Please install at least Node v8.x -- https://nodejs.org
 :eof
 pause > nul
